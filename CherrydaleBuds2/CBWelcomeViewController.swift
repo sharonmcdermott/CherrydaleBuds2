26840fe523cfd5e6e294540a82c2c0e6cc00cb82
sharon mcdermott <sharonmcdermott@me.com>
2017-08-22 14:38:39 -0400

    Added CBWelcomeViewController and CBTableViewController and navigation controller as well as a tableview and prototype cell.

---
+++ CherrydaleBuds2/CBWelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DBWelcomeViewController.swift
+//  CherrydaleBuds2
+//
+//  Created by sharon mcdermott on 8/22/17.
+//  Copyright © 2017 Sharon McDermott. All rights reserved.
+//
+
+import UIKit
+
+class CBWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
